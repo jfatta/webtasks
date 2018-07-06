483f08f9729eeaa48f9a8aba3f82602e4356395b
Jorge L. Fatta <jorge.fatta@southworks.net>
2018-07-06 19:45:33 -0300

    debugging from vscode

--- foo.js
+++ foo.js
@@ -1,3 +1,25 @@
-module.exports = function(cb) {
-    cb(null, 'hello world');
+const octokit = require('@octokit/rest')()
+
+
+module.exports = function(context, cb) {
+    
+    // token (https://github.com/settings/tokens)
+    octokit.authenticate({
+        type: 'token',
+        token: context.secrets.ghtoken
+    });
+
+    console.log(context.body.payload.repository);
+    //var owner = context.body.payload.repository.owner.login;
+    //var repo = context.body.payload.repository.name;
+    //var issueNumer = context.body.payload.issue.number;
+
+    //console.log(`Adding labels to the issue ${issueNumber} of the repo ${owner}/${repo}`)
+    //var labels = ['ack'];
+    
+    (async () => {
+        //const result = await octokit.issues.addLabels({owner, repo, issueNumer, labels });
+    
+        cb(null, `Label added to the issue`);
+    })(); 
 }
